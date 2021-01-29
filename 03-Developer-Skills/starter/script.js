cd63b7ed10aced376541229033d8a2652134a38b
smilehae <mihaepyun@gmail.com>
2021-01-29 16:58:29 +0900

    Udemy Part 3 개념 + Coding Practice 1 끝

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,19 @@
 // Remember, we're gonna use strict mode in all scripts now!
+//Develper Skills : Code Challenge 1
 'use strict';
 
+let data = [17, 21, 23];
+let data2 = [12, 5, -5, 0, 4];
+
+const printForecase = function (arr) {
+  let temp = [];
+  let dataString = '';
+  for (let i = 0; i < arr.length; i++) {
+    temp.push(arr[i]);
+    dataString += `...${arr[i]}°C in ${i + 1} days`;
+  }
+  return dataString + '...';
+};
+
+console.log(printForecase(data));
+console.log(printForecase(data2));
