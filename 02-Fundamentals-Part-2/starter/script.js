2f373fcd9531bd77a1edf5ba8a3824c0a50625ce
smilehae <mihaepyun@gmail.com>
2021-01-29 14:10:28 +0900

    Udemy 강의 Part 2 : Javascript 기초 끝!

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
 
 ///////////////////////////////
-/*
+
 // use strict 사용
 'use strict'; // 가장 위에 써야함!
 //accidental error 을 막아줌. ( 에러를 만드는 특정 일들을 못하게 막음) + 에러 보여줌
@@ -12,9 +12,9 @@ function logger() {
 logger();
 logger();
 logger();
-*/
+
 //////////////////////////////////////////////////
-/*
+
 //함수 선언 : function Declaration & Finction expresson
 const age1 = calcAge1(1999); //가능
 const age2 = calcAge2(1999); //불가능
@@ -36,9 +36,9 @@ console.log(age1, age2);
 
 //두 함수의 차이는, 선언 전에 해당 함수를 사용할 수 있는지에 따라 달라진다.
 //Fucntion Declare은 사용 가능, function expression은 불가능
-*/
+
 //////////////////////////////////////////
-/*
+
 //Arrow function : return 함수를 안써도 되도록 줄여준다.
 const calcAge3 = birthYear => 2021 - birthYear;
 
@@ -53,9 +53,9 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 
 console.log(yearsUntilRetirement(1999, 'mihae'));
-*/
+
 //////////////////////////////////////////
-/*
+
 // coding Challenge 1
 let Dolphins1 = 44, Dolphins2 = 23, Dolphins3 = 71;
 let Koalas1 = 65, Koalas2 = 54, Koalas3 = 49;
@@ -82,9 +82,9 @@ const checkWinner = function (DolphinAvg, KoalasAvg) {
 }
 
 console.log(checkWinner(avgDolhins, avgKoalas));
-*/
+
 //////////////////////////////////////////
-/*
+
 ARRAY
 
 //보편적인 방법
@@ -106,9 +106,9 @@ const years = [1999, 1967, 2018];
 const calcAge = birthYear => 2021 - birthYear;
 
 console.log(calcAge(years[1]));
-*/
+
 // /////////////////////////////////////
-/*
+
 // ARRAY METHOD
 const friends = ['Mihae', 'Juhyun'];
 //push를 하면 하나 추가
@@ -130,9 +130,9 @@ console.log(friends.indexOf('Juhyun'));
 console.log(friends.includes('Juhyun'));
 console.log(friends.includes('23'));
 
-*/
+
 //////////////////////////////
-/*
+
 Coding Challenge2
 
 let biil = 100;
@@ -153,9 +153,9 @@ let tips = new Array(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]));
 let totals = new Array(bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]);
 
 console.log(totals);
-*/
+
 ///////////////////////////////////////////////////
-/*
+
 //Object
 
 // array는 [] 로 접근하며, index로만 접근하며 각 인덱스마다 이름을 정해줄 수 없다. 따라서 순서가 중요하다.
@@ -201,8 +201,8 @@ console.log(jonas);
 //"Mihae has 3 friends, and her best friend is called juhyun"
 
 console.log(jonas.firstName + ' has ' + jonas.friends.length + " friends, and her best friend is called " + jonas.friends[1]);
-*/
-///////////////////////////////////////////
+
+//////////////////////////////////////
 //Object Methods
 
 //Object 안에 함수(메소드) 넣기
@@ -229,3 +229,128 @@ const jonas = {
 console.log(jonas.calcAge());
 console.log(jonas.age);
 console.log(jonas.getSummary());
+
+///////////////////////////////////
+//Coding Challenge3
+
+const Mark = {
+    firstName: 'Mark',
+    lastName: 'Miler',
+    mass: 78,
+    height: 1.69,
+
+    calcBMI: function () {
+        this.BMI = this.mass / (this.height ** 2);
+        return this.BMI;
+    }
+}
+
+const John = {
+    firstName: 'John',
+    lastName: 'Smith',
+    mass: 92,
+    height: 1.95,
+
+    calcBMI: function () {
+        this.BMI = this.mass / (this.height ** 2);
+        return this.BMI;
+    }
+}
+Mark.calcBMI();
+John.calcBMI();
+
+if (Mark.BMI > John.BMI) {
+    console.log(`Mark's BMI(${Mark.BMI}) is higher than John's(${John.BMI})`);
+}
+else if (Mark.BMI < John.BMI) {
+    console.log(`John's BMI(${Jonh.BMI}) is higher than Mark's(${Mark.BMI})`);
+}
+else {
+    console.log(`they both have same BMI(${Mark.BMI})`);
+}
+
+//////////////////////////////////////////////
+
+//Loop Iteration
+
+const jonas = ['mihae', 'pyun', 23, 'student', ['me', 'you']];
+
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+    console.log(jonas[i], typeof (jonas[i]));
+
+    //이 둘다 같은 뜻!
+    //types[i] = typeof (jonas[i]);
+    types.push(typeof (jonas[i]));
+}
+
+console.log(types);
+
+const years = [1975, 1972, 1999];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2021 - years[i]);
+}
+
+console.log(ages);
+
+console.log("----ONLY STRING----");
+// for (let i = 0; i < jonas.length; i++) {
+//     if (typeof jonas[i] !== 'string') continue;
+
+//     console.log(jonas[i], typeof (jonas[i]));
+// }
+
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+
+    console.log(jonas[i], typeof (jonas[i]));
+}
+
+
+///////////////////////////////////////////
+
+
+//Coding Challenge 4
+
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
+
+const calcTip = function (bill) {
+    let tip;
+
+    if (bill >= 50 && bill <= 300) {
+        tip = bill * 0.15;
+    }
+    else {
+        tip = bill * 0.2;
+    }
+
+    tips.push(tip);
+    let total = bill + tip;
+    totals.push(total);
+
+}
+
+for (let i = 0; i < 10; i++) {
+    calcTip(bills[i]);
+}
+
+console.log(tips);
+console.log(totals);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    let avg = sum / arr.length;
+    return avg;
+}
+
+console.log(calcAverage(totals));
+
+
