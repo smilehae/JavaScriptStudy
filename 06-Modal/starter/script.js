953d318a01024288c2fc3eb1f10418371545d396
smilehae <mihaepyun@gmail.com>
2021-01-30 23:27:02 +0900

    Project2 _ 내멋대로 코딩 완료. key 입력 확인 받는 절차를 확인할 필요가 있따

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const ShowModal = function () {
+  document.querySelector('.modal').classList.remove('hidden');
+  document.querySelector('.overlay').classList.remove('hidden');
+};
+
+const AddHiden = function () {
+  document.querySelector('.modal').classList.add('hidden');
+  document.querySelector('.overlay').classList.add('hidden');
+};
+
+//이렇게 하면 이 클래스의 첫번째 요소한테만 적용된다!
+//document.querySelector('.show-modal').addEventListener('click', ShowModal);
+
+//여러 항목에 접근하려면 querySelectorAll 사용
+let showModalBtns = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < showModalBtns.length; i++) {
+  showModalBtns[i].addEventListener('click', ShowModal);
+}
+
+document.querySelector('.close-modal').addEventListener('click', AddHiden);
+
+let mainSpace = document.querySelector('html');
+mainSpace.addEventListener('keydown', function (e) {
+  if (e.keyCode === 27) {
+    AddHiden();
+  }
+});
