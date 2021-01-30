78da7e4005db803e0b337d2eb8fc111b32a9e761
smilehae <mihaepyun@gmail.com>
2021-01-30 23:45:15 +0900

    Proejct2 _ 바꾸기 완료. key입력 받을땐 e.key 를 스트링값과 비교하면 된다!

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,30 +1,35 @@
 'use strict';
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const showModalBtns = document.querySelectorAll('.show-modal');
+const closeModal = document.querySelector('.close-modal');
+
 const ShowModal = function () {
-  document.querySelector('.modal').classList.remove('hidden');
-  document.querySelector('.overlay').classList.remove('hidden');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
 const AddHiden = function () {
-  document.querySelector('.modal').classList.add('hidden');
-  document.querySelector('.overlay').classList.add('hidden');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
 //이렇게 하면 이 클래스의 첫번째 요소한테만 적용된다!
 //document.querySelector('.show-modal').addEventListener('click', ShowModal);
 
 //여러 항목에 접근하려면 querySelectorAll 사용
-let showModalBtns = document.querySelectorAll('.show-modal');
 
 for (let i = 0; i < showModalBtns.length; i++) {
   showModalBtns[i].addEventListener('click', ShowModal);
 }
 
-document.querySelector('.close-modal').addEventListener('click', AddHiden);
+closeModal.addEventListener('click', AddHiden);
 
-let mainSpace = document.querySelector('html');
-mainSpace.addEventListener('keydown', function (e) {
-  if (e.keyCode === 27) {
+overlay.addEventListener('click', AddHiden);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
     AddHiden();
   }
 });
