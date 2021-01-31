b17a4cd8c6992677333db76decbee4181d1a6610
smilehae <mihaepyun@gmail.com>
2021-01-31 19:38:48 +0900

    Part9 . 모던 operator 등 배우는 중 / 현재 destructuring 배움

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,110 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  //변수로 오브젝트를 주는 순간, 바로 deconstructure해서 각각의 값을 전해준다. 단, 이때 해당 속성들이 parameter로 전달받은 object의 이름과 일치해야한다.
+  // 아래에 쓴 것 : orderDelivery: function (obj){} -> obj를 인수로 받음
+  //따라서 function 다음 괄호에 function ({obj의 변수들을 적는다})
+
+  orderDelivery: function ({
+    mainIndex = 0,
+    starterIndex = 0,
+    time = '20:00',
+    address = 'ha',
+  }) {
+    console.log(
+      `Order received!. ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time} `
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+/////////////////////////////////////////////////////
+/*
+//Desturcutre Object : API 가져올때 유용
+//{} 를 사용하며, property를 정확히 쓰는 것이 중요하다.
+
+//property 이름 그대로 사용하기
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+//property에 원하는 이름 넣기
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//default 값 설정
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//mutataing variable : 내가 만든 변수에 객체의 특정 값 받기
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+const { a: apple, b: banana } = obj;
+console.log(apple, banana);
+
+//nested Objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+*/
+/////////////////////////////////////////////
+//Destructure array
+/*
+const arr = [2, 3, 4];
+//이렇게 변수를 꺼내오려면, 한개씩 꺼내올 수 밖에 없다.
+const a = arr[0];
+
+//이런식으로 destructure 하면, 한번에 여러개를 꺼내올 수 있다.
+//단, 이러려면 const로 선언해야 한다.
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+//이렇게 하면 객체에 있는 내용도 가져올 수 있다.
+let [first, second] = restaurant.categories;
+console.log(first, second);
+
+//건너뛰기를 할때는 빈칸으로 놔두면 된다(,  ,)
+const [, , third, fourth] = restaurant.categories;
+console.log(third, fourth);
+
+//두 변수의 값을 바꾸는 방법 1  temp 만들어서 저장 후 변경
+
+// const temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+// //두 변수의 값을 바꾸는 방법 2  temp 없이 [] 이용해서 변경
+[first, second] = [second, first];
+console.log(first, second);
+
+//객체의 여러 속성의 내용 동시에 가져오기 : 함수 만들어서 배열 리턴
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+//배열 안의 배열을 deconstructing 하기 (nested value)
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default 값 설정 : API로 값 가져올때 등에 유용하다고 한다
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+*/
+
+/////////////////////////////////////////////
+//Destructuring Object
